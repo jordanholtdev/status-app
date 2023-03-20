f2062d9e9c6c49c7093d94e941ca8296d1e1cd08
Jordan <jordan.su@protonmail.com>
2023-03-20 18:07:58 -0400

    CU-860q9gp31[complete] basic user account layout

--- src/Account.jsx
+++ src/Account.jsx
@@ -55,55 +55,99 @@ export default function Account({ session }) {
     }
 
     return (
-        <form onSubmit={updateProfile} className='form-widget'>
-            <div>
-                <label htmlFor='email'>Email</label>
-                <input
-                    id='email'
-                    type='text'
-                    value={session.user.email}
-                    disabled
-                />
+        <div className='overflow-hidden bg-white shadow sm:rounded-lg'>
+            <div className='px-4 py-5 sm:px-6'>
+                <h3 className='text-base font-semibold leading-6 text-gray-900'>
+                    Account Settings
+                </h3>
+                <p className='mt-1 max-w-2xl text-sm text-gray-500'>
+                    Personal details.
+                </p>
             </div>
-            <div>
-                <label htmlFor='username'>Name</label>
-                <input
-                    id='username'
-                    type='text'
-                    required
-                    value={username || ''}
-                    onChange={(e) => setUsername(e.target.value)}
-                />
-            </div>
-            <div>
-                <label htmlFor='website'>Website</label>
-                <input
-                    id='website'
-                    type='website'
-                    value={website || ''}
-                    onChange={(e) => setWebsite(e.target.value)}
-                />
-            </div>
+            <div className='border-t border-gray-200'>
+                <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
+                    <form onSubmit={updateProfile} className='form-widget'>
+                        <div className='overflow-hidden shadow sm:rounded-md'>
+                            <div className='bg-white px-4 py-5 sm:p-6'>
+                                <div className='grid grid-cols-4 gap-6'>
+                                    <div className='col-span-6 sm:col-span-3'>
+                                        <label
+                                            htmlFor='email'
+                                            className='block text-sm font-medium leading-6 text-gray-900'
+                                        >
+                                            Email:
+                                        </label>
+                                        <input
+                                            id='email'
+                                            type='text'
+                                            value={session.user.email}
+                                            disabled
+                                            className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                        />
+                                    </div>
+                                    <div className='col-span-6 sm:col-span-3'>
+                                        <label
+                                            htmlFor='username'
+                                            className='block text-sm font-medium leading-6 text-gray-900'
+                                        >
+                                            Name:
+                                        </label>
+                                        <input
+                                            id='username'
+                                            type='text'
+                                            required
+                                            value={username || ''}
+                                            onChange={(e) =>
+                                                setUsername(e.target.value)
+                                            }
+                                            className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                        />
+                                    </div>
+                                    <div className='col-span-6 sm:col-span-3'>
+                                        <label
+                                            htmlFor='website'
+                                            className='block text-sm font-medium leading-6 text-gray-900'
+                                        >
+                                            Website
+                                        </label>
+                                        <input
+                                            id='website'
+                                            type='website'
+                                            value={website || ''}
+                                            onChange={(e) =>
+                                                setWebsite(e.target.value)
+                                            }
+                                            className='mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+                                        />
+                                    </div>
 
-            <div>
-                <button
-                    className='button block primary'
-                    type='submit'
-                    disabled={loading}
-                >
-                    {loading ? 'Loading ...' : 'Update'}
-                </button>
-            </div>
+                                    <div className='col-span-6 sm:col-span-3'>
+                                        <button
+                                            className='inline-flex justify-center rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+                                            type='submit'
+                                            disabled={loading}
+                                        >
+                                            {loading ? 'Loading ...' : 'Save'}
+                                        </button>
+                                    </div>
 
-            <div>
-                <button
-                    className='button block'
-                    type='button'
-                    onClick={() => supabase.auth.signOut()}
-                >
-                    Sign Out
-                </button>
+                                    <div className='col-span-6 sm:col-span-3'>
+                                        <button
+                                            type='button'
+                                            onClick={() =>
+                                                supabase.auth.signOut()
+                                            }
+                                            className='inline-flex justify-center rounded-md bg-slate-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+                                        >
+                                            Sign Out
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+        </div>
     );
 }
