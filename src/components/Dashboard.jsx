df1e7b2326efc0294be49e02cf54eca02a6dab58
Jordan <jordan.su@protonmail.com>
2023-03-21 11:43:18 -0400

    CU-860q9hjhg[in progress] add Account button

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -46,7 +46,7 @@ export default function Dashboard({ session }) {
                 <div className='mx-auto max-w-7xl px-4 sm:px-6 lg:px-8'>
                     <div className='flex h-16 items-center justify-between'>
                         <div className='flex items-center'>
-                            <div className='hidden md:block'>
+                            <div>
                                 <div className='ml-10 flex items-baseline space-x-4'>
                                     <button
                                         type='button'
@@ -54,6 +54,12 @@ export default function Dashboard({ session }) {
                                         className='inline-flex justify-center rounded-md py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
                                     >
                                         Sign Out
+                                    </button>
+                                    <button
+                                        type='button'
+                                        className='inline-flex justify-center rounded-md py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500'
+                                    >
+                                        Account
                                     </button>
                                 </div>
                             </div>
