3a3a05c11d28175caa673ec6c5c757316d132be4
Jordan <jordan.su@protonmail.com>
2023-03-23 10:17:59 -0400

    CU-860qaj0dv[complete] - create list component

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,10 +1,72 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../supabaseClient';
 import Dashboard from '../components/Dashboard';
 import PropTypes from 'prop-types';
 
 const List = ({ session }) => {
+    const [loading, setLoading] = useState();
+    const [flights, setFlights] = useState([]);
+
+    // fetch the saved flights from db
+    // fetch only flights belonging to user
+    useEffect(() => {
+        async function getSavedFlights() {
+            setLoading(true);
+            const { user } = session;
+
+            let { data, error } = await supabase
+                .from('testing')
+                .select(`name, info`)
+                .eq('user_id', user.id);
+
+            if (error) {
+                console.warn(error);
+            } else if (data) {
+                console.log(data);
+                setFlights(data);
+            }
+
+            setLoading(false);
+        }
+
+        getSavedFlights();
+    }, [session]);
+
     return (
         <div>
             <Dashboard key={session.user.id} session={session} />
+            <div className='px-4 py-5 sm:px-6 max-w-md'>
+                <h3 className='text-base font-semibold leading-6 text-white'>
+                    Add flight
+                </h3>
+                <p className='mt-1 max-w-2xl text-sm text-gray-500'>
+                    Select a flight to track.
+                </p>
+            </div>
+            <div className='border-t border-zinc-600'>
+                <div className='flex justify-center min-w-full mt-10 '>
+                    {loading ? (
+                        <div>testing</div>
+                    ) : (
+                        <div>
+                            <div className='sm:columns-1 w-60'>
+                                {flights.map((flight, idx) => (
+                                    <div
+                                        key={idx}
+                                        className='container mx-auto overflow-hidden bg-zinc-800 shadow rounded-lg py-1 mt-4 w-full ring-1 ring-gray-700'
+                                    >
+                                        <div className='px-4 py-5 sm:px-6'>
+                                            <p className='mt-1 max-w-2xl text-sm text-gray-400'>
+                                                {flight.name}
+                                            </p>
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
