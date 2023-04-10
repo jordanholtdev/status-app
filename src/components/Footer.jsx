adfdba9c60ea31f2b1793c90d73f754e1753e471
Jordan <jordan.su@protonmail.com>
2023-04-09 21:53:43 -0400

    CU-860q9x9j1[complete] - Create a footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,8 @@
 const Footer = () => {
     return (
-        <div className='px-4 py-5 sm:px-6 max-w-md'>
-            <div className='text-base font-semibold leading-6 text-white'>
-                Footer
+        <div className='px-4 py-5 min-w-full sm:px-6 max-w-6xl'>
+            <div className='mt-1 text-sm text-gray-500 text-center'>
+                Made with 🧠 by Jordan Holt
             </div>
         </div>
     );
